f2ced03b402b8f89003a5851687d8745038fda9c
vasily-shapenko <v.shapenko@payonline.ru>
2018-02-01 16:30:23 +0300

    Rewritten using new concept

--- HttpWrapper/Program.fs
+++ HttpWrapper/Program.fs
@@ -1,51 +1,76 @@
 ﻿open System.Net.Http
 open System
+open System.Web
 
-type Content=unit->HttpContent
+type RequestParameter=
+    |Header of (unit->string*string)
+    |Content of (unit->HttpContent)
+    |Query of (unit->string*string)
 
-type RequestOperation<'ctx,'req>=
-|Operation of ('ctx->'req->'req)
-|Pure of 'req
-
-let mapI f=function
-|Operation next->Operation(next>>f)
+type RequestParameters=RequestParameters of RequestParameter list
 
 
-type RequestBuilder (method:HttpMethod,url:string,content:Content option)=
-      member this.Delay(f)=f()
+let setHeader (request:HttpRequestMessage) (header:unit->string*string)=
+ let k,v=header()
+ request.Headers.Add(k,v)
 
-      [<CustomOperation("header")>]
-      member this.Header(f:HttpRequestMessage->HttpRequestMessage,name:string,value:string)=
-        f>>fun r->
-           r.Headers.Add(name,value)
-           r 
-      [<CustomOperation("content")>]
-      member this.Content(f:HttpRequestMessage->HttpRequestMessage,content:unit->HttpContent)=
-         f>> fun r->
-            r.Content<-content()
-      member this.Yield (())=fun (r:HttpRequestMessage)->   
-                              r.Method<-method
-                              r.RequestUri<-new Uri(url)
-                              r
+let setContent (request:HttpRequestMessage) (content:unit->HttpContent)=
+  request.Content<-content()
+
+let setQueryParam (request:HttpRequestMessage) (query:unit->string*string)=
+  let builder=new UriBuilder(request.RequestUri)
+  let queryCol=HttpUtility.ParseQueryString(builder.Query)
+  let k,v=query()
+  queryCol.[k]<-v
+  builder.Query<-queryCol.ToString()
+  request.RequestUri<-builder.Uri
 
 
 
-let create method url content=RequestBuilder(method,url,content)
+type RequestBuilder()=
+    [<CustomOperation("header")>]
+    member this.Header(RequestParameters source,key,value)= RequestParameters[ yield! source 
+                                                                               yield Header (fun()->(key,value))]
 
-let Get url=create HttpMethod.Get url None
-let Post url f =create HttpMethod.Post url (Some f)
-let Put url=create HttpMethod.Put url
-let Delete url=create HttpMethod.Delete url None
-let Options url content=create HttpMethod.Options url content
+    [<CustomOperation("query")>]
+    member this.Query(RequestParameters source,key,value)= RequestParameters[ yield! source
+                                                                              yield  Query (fun()->(key,value))]
+
+    [<CustomOperation("content")>]
+    member this.Content(RequestParameters source,content)= RequestParameters[ yield! source
+                                                                              yield Content (fun()->content)]
+
+    member x.Yield (()) = RequestParameters []
 
 
+let create method (url:string) builder=
+   let message= new HttpRequestMessage(method,url)
+   match builder with
+   |RequestParameters l->l|>List.fold(fun acc x->match x with
+                                                  |Header f->setHeader acc f
+                                                  |Content c->setContent acc c
+                                                  |Query q->setQueryParam acc q
+                                                 acc) message
+   
 
-       
+let http=RequestBuilder()
+
+let Get url builder=create HttpMethod.Get url builder
+let Post url builder =create HttpMethod.Post url builder
+let Put url builder=create HttpMethod.Put url builder
+let Delete url builder=create HttpMethod.Delete url builder
+let Options url builder =create HttpMethod.Options url builder
+
+ 
 [<EntryPoint>]
 let main argv = 
-    let request =Get "http://lenta.ru" {
-                                        header "h1" "v1"
-                                        header "h2" "v2"
-                                       }
+    let b=http{
+                header "a" "b"     
+                header "c" "d"  
+                query "1" "2"
+               } 
+    let request =Get "http://lenta.ru" b                                         
+    let client=new HttpClient()
+    let data=client.SendAsync(request).Result.Content.ReadAsStringAsync().Result
 
     0 // return an integer exit code
